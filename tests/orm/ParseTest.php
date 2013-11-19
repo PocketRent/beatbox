d2ba1b73fc2f2ee8ed92c9d39445540ad06dd819
Simon Welsh <simon@pocketrent.com>
2013-11-19 22:20:06 +0000

    Add a test to cover parsing the composite value returned from an array

--- tests/orm/ParseTest.php
+++ tests/orm/ParseTest.php
@@ -135,4 +135,8 @@ class ParseTest extends beatbox\Test {
 	public function testCompositeNestedString() {
 		$this->assertEquals(Vector {'("123""456")', '1'}, db_parse_composite('("(""123""""456"")",1)'));
 	}
+
+	public function testCompositeFromArray() {
+		$this->assertEquals(Vector { "abc" }, db_parse_composite('"(abc)"'));
+	}
 }

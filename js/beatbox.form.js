2a2994f49ebce383178e88260bc171bdfe4d62c9
James Miller <james@pocketrent.com>
2013-11-20 00:03:55 +0000

    Add client-side form handling

---
+++ js/beatbox.form.js
@@ -0,0 +1,45 @@
+(function ($, window) {
+	'use strict';
+
+	$.entwine('beatbox', function ($) {
+		$('.popup form,form.ajax-form').entwine({
+			onsubmit: function (e) {
+				e.preventDefault();
+				var action = this.attr('action');
+				var uri = new Uri(action);
+				var fragment = uri.params['fragments'];
+				var that = this;
+				this.ajaxSubmit({
+					success: function (data, status) {
+						if (status == 'success') {
+							data = data[fragment];
+
+							if (data) {
+								that.handleData(data);
+								return;
+							}
+						}
+
+						// Error handling
+					}
+				});
+			},
+			handleData: function(data) {
+				if (typeof data == 'string') {
+					this.replaceWith(data);
+				} else if (typeof data == 'object') {
+					var fragments = data['fragments'];
+					if (fragments) {
+						loadFragments(fragments.join(','));
+					}
+
+					if (Popup.currentPopup()) {
+						Popup.currentPopup().close();
+					}
+				}
+
+			}
+		});
+	});
+
+})(jQuery, this);

6d5696ba7c3017f2571d5bce5cdcbed5963707c6
Simon Welsh <simon@pocketrent.com>
2014-04-17 22:59:18 +0000

    add_task returns an int

--- php/functions/tasks/Task.php
+++ php/functions/tasks/Task.php
@@ -3,6 +3,6 @@
 /**
  * Create a task and add it to the queue
  */
-function add_task((function (...): void) $callback, ...) : void {
-	(new ReflectionClass('beatbox\Task'))->newInstanceArgs(func_get_args())->queue();
+function add_task((function (...): void) $callback, ...) : int {
+	return (new ReflectionClass('beatbox\Task'))->newInstanceArgs(func_get_args())->queue();
 }
